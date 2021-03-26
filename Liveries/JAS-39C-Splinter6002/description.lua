9dc49c568bc47a4e8cd24256ad57993be2ffcd09
FatSpacePanda <fatspacepandatv@gmail.com>
2021-03-26 12:11:06 +0100

    Textures/Livery
    New version of the Splinter camo, with roughmet
    Removed some duplicate texture files - Did not touch any files from liveries Im not sure what files it uses
    Added liveries to both versions, aa and ag
    Tweaked color of Mat3 for digicamo
    Added cockpit textures for mfds
    renamed a file that was in swedish

--- Liveries/JAS-39C-Splinter6002/description.lua
+++ Liveries/JAS-39C-Splinter6002/description.lua
@@ -1,20 +1,19 @@
 livery = {
     {"VSN_JAS39_Mat1", 0, "VSN_JAS39_Mat1", false},
-	{"VSN_JAS39_Mat1_RoughMet", ROUGHNESS_METALLIC ,"VSN_JAS39_Mat1_RoughMet ",false},
-	{"VSN_JAS39_Mat1_B", 0, "VSN_JAS39_Mat1_B", false},
-    
+	{"VSN_JAS39_Mat1", ROUGHNESS_METALLIC ,"VSN_JAS39_Mat1_RoughMet",false};
+	{"VSN_JAS39_Mat1_B", 0, false},
     {"VSN_JAS39_Mat2", 0, "VSN_JAS39_Mat2", false},
+	{"VSN_JAS39_Mat2", 1,  ROUGHNESS_METALLIC ,"VSN_JAS39_Mat2_RoughMet",false};
 	{"VSN_JAS39_Mat2_B", 0, "VSN_JAS39_Mat2_B", false},
-	{"VSN_JAS39_Mat2_RoughMet", ROUGHNESS_METALLIC ,"VSN_JAS39_Mat2_RoughMet",false},
     {"VSN_JAS39_Mat3", 0, "VSN_JAS39_Mat3", false},
-	{"VSN_JAS39_Mat3_B", 0, "VSN_JAS39_Mat3_B", false},
-	{"VSN_JAS39_Mat3_RoughMet", ROUGHNESS_METALLIC ,"VSN_JAS39_Mat3_RoughMet",false},
+	{"VSN_JAS39_Mat3_B", 1,  false},
+	{"VSN_JAS39_Mat3",0, ROUGHNESS_METALLIC ,"VSN_JAS39_Mat3_RoughMet",false};
     {"VSN_JAS39_Triebwerk", 0, "VSN_JAS39_Triebwerk", false},
-	{"VSN_JAS39_Triebwerk_B", 0, "VSN_JAS39_Triebwerk_B", false},
-	{"VSN_JAS39_Triebwerk_RoughMet", ROUGHNESS_METALLIC ,"VSN_JAS39_Triebwerk_RoughMet",false},
+	{"VSN_JAS39_Triebwerk",0, ROUGHNESS_METALLIC ,"VSN_JAS39_Triebwerk_RoughMet",false};
+	{"VSN_JAS39_Triebwerk_B", 0,  false},
   
 }
-name = "JAS-39C-Splinter6002"
-name_cn = "FC-1 枭龙06架"
+name = "JAS-39C-Splinter-6002"
+name_cn = "JAS-39C-Splinter-6002"
 
 ---countries = {"CHN"}
