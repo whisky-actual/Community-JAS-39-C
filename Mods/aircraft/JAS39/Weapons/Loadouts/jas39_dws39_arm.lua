04ca9330f340a1544047139a2296005b92ae16a3
searching46dof <107732149+searching46dof@users.noreply.github.com>
2025-03-08 19:36:08 -0500

    Fix syntax problem preventing spawning aircraft carriers
    Fix syntax problem which prevents spawning assets such as aircraft carriers in DCS F/A-18c instant action case1 and case3 missions.
    The fix was mentioned by Urbi in DCS forum thread - cj43g3r a.k.a. searching46dof

--- Mods/aircraft/JAS39/Weapons/Loadouts/jas39_dws39_arm.lua
+++ Mods/aircraft/JAS39/Weapons/Loadouts/jas39_dws39_arm.lua
@@ -185,7 +185,7 @@ JAS_DWS_39_ARM =
     
     launcher =
     {
-        cluster = cluster_desc("Bomb_Other", wsType_Bomb_Cluster, 
+        cluster = cluster_desc("Bomb_Other", wsType_Bomb_Cluster, levParam),
         {
             scheme = 
             {
